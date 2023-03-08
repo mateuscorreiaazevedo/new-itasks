5812d79d24e1dc0717aab1cda1beeaa094f1a168
Mateus <mateuscorreiaazevedo@gmail.com>
2023-03-08 18:39:34 -0300

    feat: new branch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { SessionProvider } from 'next-auth/react'
-import { ThemeProvider } from '@/modules/core'
-import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
+import { ThemeProvider } from '@/modules/core'
 import 'react-toastify/dist/ReactToastify.css'
-
+import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
 
 export default function App ({ Component, pageProps }: AppProps) {
